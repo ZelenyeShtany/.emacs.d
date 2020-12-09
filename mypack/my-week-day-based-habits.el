32550d51e4ffa4a2427968139d576bb915895601
zelenyeshtany <zelenyeshtany@gmail.com>
2020-12-10 04:18:43 +0600

    my org habits update (table view)

--- mypack/my-week-day-based-habits.el
+++ mypack/my-week-day-based-habits.el
@@ -275,5 +275,258 @@ Create it if it doesnt exist"
   )
 (add-hook 'org-after-todo-state-change-hook 'my/set-next-week-day)
 
+(defun my-org-table/create-empty-schedule-table ()
+  (interactive)
+  (let*
+      (
+       ;;(table-start-time (ts-hour (ts-parse "08:00")))
+       (table-start-time (ts-parse "08:30"))
+
+       (table-end-time (ts-parse "23:00"))
+       
+       (ts-minute (ts-adjust 'hour +1 'minute +30 (ts-parse "08:00")))
+       (headers
+	(list
+	 "TIME"
+	 "Mon"
+	 "Tue"
+	 "Wed"
+	 "Thu"
+	 "Fri"
+	 "Sat"
+	 "Sun"))
+       (time-unit-iterator table-start-time)
+       (rows-number
+	(+
+	 3
+	 (if (not (eq (ts-minute table-end-time) 0)) 1 0)
+	 (if (not (eq (ts-minute table-start-time) 0)) -1 0)
+	 (* 2 (- (ts-hour table-end-time)
+		(ts-hour table-start-time)
+		)))
+	)
+       )
+    ;;(insert "|" (string-join headers "|") "|\n")
+    ;;(insert "|" ())
+    ;;(org-table-create-with-table.el ) ;; OR (table-insert)
+
+    ;; create table
+    (table-insert (length headers) rows-number)
+
+    ;; fill up headers
+    (dolist (header headers)
+      (table--cell-insert-char header)
+      (table-forward-cell)
+      )
+
+    ;; fill up time units
+    (table--cell-insert-char
+       "Unspecified time"
+       )
+    
+    ;; move down by 1 cell
+    (cl-dotimes (iter (length headers))
+	(table-forward-cell)
+	)
+    
+    (while (ts<= time-unit-iterator table-end-time)
+      (table--cell-insert-char
+       (concat
+	(number-to-string (ts-hour time-unit-iterator))
+	":"
+	(number-to-string (ts-minute time-unit-iterator))
+	)
+       )
+
+      ;; move down by 1 cell
+      (cl-dotimes (iter (length headers))
+	(table-forward-cell)
+	)
+      ;; increment time-unit-iterator by 30 minutes
+      (setq time-unit-iterator (ts-adjust 'minute +30 time-unit-iterator))
+      )
+
+
+    )
+  )
+;;(org-element-property :title (org-element-at-point))
+(defun my-org-habits/find-recur-tasks-by-day(day)
+  (let*
+      (
+       (regex "\\* +|.*?Mon.*?|")
+       
+       (tasks
+	(let*
+	    (
+	     (headings-titles (list))
+	     )
+	  (save-window-excursion
+	    (find-file (concat my-org-directory "regular.org"))
+	    (widen)
+	    (goto-char (point-min))
+	    (while (re-search-forward (concat "^\\*+ +.*?|.*?" day ".*?|") nil t)
+	      (push (org-element-property :title (org-element-at-point)) headings-titles)
+	      ))
+	    headings-titles)))
+    tasks
+    )
+  )
+
+
+(defun my-org-table/goto-cell (row-number column-number)
+  (let*
+      (
+       (columns-in-table 8)
+       (times (+ column-number -1
+		 (* (1- row-number) columns-in-table)
+		 ))
+       )
+    (table-goto-top-left-corner)
+    (next-line)
+    (right-char)
+    (table-forward-cell times)
+      )
+    )
+
+(defun my-org-table/get-cell-value (row column)
+  ;; TODO dumb code
+  (let*
+      (
+       (beg-of-cell-line)
+       (end-of-cell-line)
+       (cell-value "")
+       )
+    (my-org-table/goto-cell row column)
+    (while (not (my-org-table/point-at-table-horizontal-border-p))
+      (setq beg-of-cell-line (point))
+      (while (not (eq (char-after) ?|))
+	(forward-char)
+	)
+      (setq end-of-cell-line (point))
+      (setq cell-value (concat cell-value (buffer-substring-no-properties beg-of-cell-line end-of-cell-line) "\n"))
+      (next-line)
+      (while (and
+	      (not (eq (char-before) ?|))
+	      (not (eq (char-before) ?+))
+	      )
+	(forward-char -1)
+	)
+      )
+    cell-value
+    )
+  )
+
+(defun my-org-table/point-at-table-horizontal-border-p ()
+  (let*
+      (
+       (current-line (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+       (res (string-match "\\(\\+-+\\)+\\+" current-line))
+       )
+    
+    (and
+     res
+     (>= (point) (+ (match-beginning 0) (line-beginning-position)))
+     (<  (point) (+ (match-end 0) (line-beginning-position)) )
+     )
+    )
+  )
+
+(defun my-org-table/table-columns-number ()
+  (let*
+      (
+       (line-number)
+       (count 0)
+       )
+    (save-excursion
+     (my-org-table/goto-cell 1 1)
+    (setq line-number (line-number-at-pos))
+    (while (eq line-number (line-number-at-pos))
+      (table-forward-cell) (setq count (1+ count))))
+    count
+      )
+    )
+
+(defun my-org-table/table-rows-number ()
+  (let*
+      (
+       (line-number)
+       (count 0)
+       )
+    (save-excursion
+     (table-goto-top-left-corner)
+    (while (org-at-table-p t)
+      (next-line)
+      (when (eq (char-after) ?+) (setq count (1+ count)))
+      ))
+    count
+    )
+  )
+
+
+(defun my-org-table/get-column-number-by-header-value (header-value)
+  (let*
+      (
+       ()
+       ()
+       )
+    (my-org-table/goto-cell 1 1)
+    (while
+	(table-forward-cell ))
+      )
+    )
+
+(defun my-org-table/append-to-unspec-time (day)
+  (let*
+      (
+       ()
+       ()
+       )
+    
+      )
+    )
+
+
+(defun my-org-table/fill-schedule-table ()
+  (let*
+      (
+       (days (list "Mon" "Tue" "Wed" "Thu" "Fri" "Sat" "Sun"))
+       (current-day-tasks-list)
+       )
+    (dolist (day days)
+      (setq current-day-tasks-list
+	    (my-org-habits/find-recur-tasks-by-day day))
+      (dolist (task current-day-tasks-list)
+	(cond
+	 ;; check for time for specific day
+	 ((string-match
+	   (concat "|.*?" day "*\\([0-9]\\{2\\}:[0-9]\\{2\\}\\(-[0-9]\\{2\\}:[0-9]\\{2\\}\\)?\\).*?|") ;; * |Mon,Tue 99:99,Fri| -- time for specific day
+	   task
+	   )
+	 BODY 
+	  
+	 )
+
+	 ;;check for general time
+	 ((string-match
+	   (concat "|.*? *\\(\\[[0-9]\\{2\\}:[0-9]\\{2\\}\\(-[0-9]\\{2\\}:[0-9]\\{2\\}\\)?\\]\\).*?|") ;; * |Mon,Tue,Fri [99:99]| -- general time
+	   task
+	   )
+	 BODY 
+	  
+	 )
+
+	 ;; no specified time in task
+	 (t
+	  ;; append to 'Unspecified time' row
+	  )
+	 
+	 )
+	)
+      )
+    )
+    )
+
+
+
 
 (provide 'my-week-day-based-habits)
