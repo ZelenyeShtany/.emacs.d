910ba44ad7198cb0e634fb981484b1ff633de73b
zelenyeshtany <zelenyeshtany@gmail.com>
2020-12-25 21:52:38 +0600

    asd

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - sonyvaio at <2020-12-23 Wed 23:03>
-(setq org-clock-stored-history '(("/data/gitserver/org/inbox.org" . 263526) ("/data/gitserver/org/notes.org" . 58059) ("/data/gitserver/org/утррит.org" . 230) ("/data/gitserver/org/notes.org" . 90589) ("/data/gitserver/org/notes.org" . 90468)))
+;; org-clock-save.el - sonyvaio at <2020-12-25 Fri 00:37>
+(setq org-clock-stored-history '(("/data/gitserver/org/notes.org" . 18113) ("/data/gitserver/org/notes.org" . 52244) ("/data/gitserver/org/notes.org" . 60077) ("/data/gitserver/org/notes.org" . 91789) ("/data/gitserver/org/notes.org" . 91605)))
